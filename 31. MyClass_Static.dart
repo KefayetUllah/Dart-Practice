80d675a106533deab3e6e0ae57a6334e29c0b211
Kefayet Ullah <150620881+KefayetUllah@users.noreply.github.com>
2025-06-09 00:45:24 +0600

    Add files via upload

---
+++ 31. MyClass_Static.dart
@@ -0,0 +1,12 @@
+class MyClass {
+  static void addTwoNumber(int a, int b) {
+    print(a + b);
+  }
+
+  static void addThreeNumber(int a, int b, int c) {
+    print(a + b + c);
+  }
+
+  static String MyName = "Kefayet Ullah";
+  static List<String> Alphabet = ['A', 'B', 'C'];
+}
