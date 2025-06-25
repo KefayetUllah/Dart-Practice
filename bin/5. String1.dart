d5401c7ba4fa331b7e219e6391a3ba26e94e061a
Kefayet Ullah <kefayet107@gmail.com>
2025-06-26 01:06:20 +0600

    String new line and tab

---
+++ bin/5. String1.dart
@@ -0,0 +1,4 @@
+void main(){
+  print('My name is Kefayet Ullah Arif \nI am a Student of Northern University Bangladesh');
+  print('Now I am in 7th Semester.\t I am Learning Dart in this Semester.');
+}
