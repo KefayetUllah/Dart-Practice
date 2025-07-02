a092ee37bc6c0773eb280c64bdd0eaeb814f5a49
Kefayet Ullah <kefayet107@gmail.com>
2025-07-03 01:20:20 +0600

    int to string

--- bin/0. Practice.dart
+++ bin/0. Practice.dart
@@ -1,10 +1,7 @@
-void main(){
-  String num = "42";
-int value = int.parse(num);  // string to int
-double d = double.parse("3.14");
-String str = value.toString();
- 
-print(value);
-print(d);
-print(str);
+void main (){
+  double a =  10.30;
+  int b = a.toInt();
+
+  print("The value of a is $a and It's type is ${a.runtimeType}");
+  print("The value of b is $b and It's type is ${b.runtimeType}");
 }
--- bin/5. Double_to_Int.dart
+++ bin/5. Double_to_Int.dart
@@ -5,3 +5,5 @@ void main() {
   print("The value of num1 is $num1. Its type is ${num1.runtimeType}");
   print("The value of num2 is $num2. Its type is ${num2.runtimeType}");
 }
+
+
