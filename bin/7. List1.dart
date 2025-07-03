81ae38dcfe30f6b2f4fed7f46c08dc8c5b043282
Kefayet Ullah <kefayet107@gmail.com>
2025-07-04 01:06:02 +0600

    List Item

--- bin/7. List1.dart
+++ bin/7. List1.dart
@@ -1,11 +1,12 @@
-void main() {
-List<String> names = ["Raj", "John", "Max"];
-print("Value of names is $names");
-print("Value of names[0] is ${names[0]}"); // index 0
-print("Value of names[1] is ${names[1]}"); // index 1
-print("Value of names[2] is ${names[2]}"); // index 2
+void main(){
+  List<String> names = ["Kefayet", "Soptok", "Rifat"];
 
-  // Finding Length of List 
-int length = names.length;  
-print("The Length of names is $length");
+  print("The value of names $names");
+  print("The value of names 0 is ${names[0]}");
+  print("The value of names 1 is ${names[1]}");
+  print("The value of names 2 is ${names[2]}");
+
+
+  int length = names.length;
+  print("The length of names is $length");
 }
