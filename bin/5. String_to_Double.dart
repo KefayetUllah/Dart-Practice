a20db9345351b555d49b87c1d4838ae87741daf1
Kefayet Ullah <kefayet107@gmail.com>
2025-07-03 22:12:30 +0600

    String to Double conversion

--- bin/5. String_to_Double.dart
+++ bin/5. String_to_Double.dart
@@ -1,8 +1,10 @@
-void main() {
-String strvalue = "1.1";
-print("Type of strvalue is ${strvalue.runtimeType}");
-double doublevalue = double.parse(strvalue);
-print("Value of doublevalue is $doublevalue");
-// this will print data type
-print("Type of doublevalue is ${doublevalue.runtimeType}");
+void main(){
+  String str1 = "1.2";
+  double doublevalue = double.parse(str1);
+
+  print("The data type is ${str1.runtimeType}");
+
+  print("The value of double is $doublevalue");
+  print("The data type is ${doublevalue.runtimeType}");
+
 }
