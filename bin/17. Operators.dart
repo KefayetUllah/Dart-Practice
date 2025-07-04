24459bcdcc4b503ca2ddd377bd9fb5e8695126f2
Kefayet Ullah <kefayet107@gmail.com>
2025-07-05 00:42:55 +0600

    Operators

---
+++ bin/17. Operators.dart
@@ -0,0 +1,18 @@
+void main (){
+    int num1 = 10;
+    int num2 = 5;
+
+    int ADD = num1+num2;
+    int SUB = num1-num2;
+    int MUL = num1*num2;
+    double DIV = num1/num2;
+    int INTDIV = num1~/num2;
+    int MOD = num1%num2;
+
+    print("The Additin is $ADD");
+    print("The sub is $SUB");
+    print("The Mul is $MUL");
+    print("The Div is $DIV");
+    print("The intDiv is $INTDIV");
+    print("The mod is $MOD");
+}
