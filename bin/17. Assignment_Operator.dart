8634390b9b4006fb882d236d497d14e65505e36c
Kefayet Ullah <kefayet107@gmail.com>
2025-07-05 01:29:32 +0600

    Assignment Operators

---
+++ bin/17. Assignment_Operator.dart
@@ -0,0 +1,15 @@
+void main(){
+  double age = 20;
+
+  age+= 1;
+  print("The age is: $age");
+
+  age-= 1;
+  print("The age is: $age");
+
+  age*= 2;
+  print("The value is: $age");
+
+  age/= 2;
+  print("The value is: $age");
+}
