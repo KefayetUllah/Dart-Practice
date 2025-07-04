f3eb3b1fbfbf034ceba88ade424911eebd1c62c7
Kefayet Ullah <kefayet107@gmail.com>
2025-07-05 01:17:20 +0600

    Increment Decrement

---
+++ bin/17. Increment_and_Decrement.dart
@@ -0,0 +1,15 @@
+void main(){
+  int num = 0;
+  int num1 = 0;
+
+
+  num1 = ++num;
+  print("The value of num1 is: $num1");
+
+  int newnum = 0;
+  int newnum1 = 0;
+
+  newnum1 = newnum++;
+  print("The value of Newnum1 is: $newnum1");
+
+}
