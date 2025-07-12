9ac0fe35994a50cb65607051c9cfde2992a89af2
Kefayet Ullah <kefayet107@gmail.com>
2025-07-13 00:32:07 +0600

    temperature

--- bin/Project_Temperature_Converter.dart
+++ bin/Project_Temperature_Converter.dart
@@ -1,5 +1,7 @@
 void main(){
   double celsius = 25.0;
   double fahrenheit = (celsius*9/5)+32;
-  print("$celsius C is equal to $fahrenheit F");
+  //print("$celsius C is equal to $fahrenheit F");
+print("${celsius.toStringAsFixed(1)} C is equal to ${fahrenheit.toStringAsFixed(1)} F");
+
 }
