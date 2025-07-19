a72836e6138f9308d28a6cc8eb6c9a66d62dd704
Kefayet Ullah <kefayet107@gmail.com>
2025-07-19 15:23:54 +0600

    Calculate time

---
+++ bin/50. Problem_Calculate_Time.dart
@@ -0,0 +1,11 @@
+/*Suppose, your distance to office from home is 25 km and you travel 40 km per hour. Write a program to calculate 
+time taken to reach office in minutes. Formula= (distance) / (speed)*/
+
+void main(){
+  int officeDistanceKM = 25;
+  int travelHour = 40;
+
+  double takenTime = officeDistanceKM/travelHour;
+
+  print("Total Time take: $takenTime");
+}
